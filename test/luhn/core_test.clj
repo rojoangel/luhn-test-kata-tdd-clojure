899e8840a7d904272a8744012e355c1f36da5cd0
Angel Rojo Marijuan <arojomarijuan@magento.com>
2017-03-22 21:45:43 +0100

    0 passes the luhn check

--- test/luhn/core_test.clj
+++ test/luhn/core_test.clj
@@ -1,3 +1,10 @@
 (ns luhn.core-test
   (:require [midje.sweet :refer :all]
             [luhn.core :refer :all]))
+
+(defn valid? [cc]
+  true)
+
+(facts "about luhn check"
+       (fact "0 passes the test"
+             (valid? "0") => true))
