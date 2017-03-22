a92a6291cf1b6c1e6b2f82bb9fea0f4e87ef046b
Angel Rojo Marijuan <arojomarijuan@magento.com>
2017-03-22 22:07:52 +0100

    000 passes the test

--- test/luhn/core_test.clj
+++ test/luhn/core_test.clj
@@ -2,13 +2,19 @@
   (:require [midje.sweet :refer :all]
             [luhn.core :refer :all]))
 
+(defn- cc->digits [cc]
+  (map (comp read-string str) cc))
+
 (defn valid? [cc]
-  (if (= "0" cc)
-    true
-    false))
+  (let [digits (cc->digits cc)]
+    (if (= 0 (apply + digits))
+      true
+      false)))
 
 (facts "about luhn check"
        (fact "0 passes the test"
              (valid? "0") => true)
        (fact "1 fails the test"                             ; introduces conditional
-             (valid? "1") => false))
+             (valid? "1") => false)
+       (fact "000 passes the test"                          ; introduces sum
+             (valid? "000") => true))
