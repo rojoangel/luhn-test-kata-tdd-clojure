2fcfe50891b4bc5df9b00bc1b9ba182da1dde253
Angel Rojo Marijuan <arojomarijuan@magento.com>
2017-03-22 23:20:54 +0100

    move production code to core ns

--- src/luhn/core.clj
+++ src/luhn/core.clj
@@ -1,1 +1,19 @@
 (ns luhn.core)
+
+(defn- cc->digits [cc]
+  (map (comp read-string str) cc))
+
+(defn- sum-digits [n]
+  (apply + (map #(read-string (str %)) (str n))))
+
+(defn valid? [cc]
+  (let [digits (cc->digits cc)
+        even-digits (take-nth 2 (rest digits))
+        reverse-digits (reverse digits)
+        reverse-odds (take-nth 2 reverse-digits)
+        reverse-evens (take-nth 2 (rest reverse-digits))
+        dupped-reverse-evens (map (partial * 2) reverse-evens)
+        digit-added-dupped-reverse-evens (map sum-digits dupped-reverse-evens)]
+    (if (= 0 (mod (+ (apply + reverse-odds) (apply + digit-added-dupped-reverse-evens)) 10))
+      true
+      false)))
--- test/luhn/core_test.clj
+++ test/luhn/core_test.clj
@@ -2,24 +2,6 @@
   (:require [midje.sweet :refer :all]
             [luhn.core :refer :all]))
 
-(defn- cc->digits [cc]
-  (map (comp read-string str) cc))
-
-(defn- sum-digits [n]
-  (apply + (map #(read-string (str %)) (str n))))
-
-(defn valid? [cc]
-  (let [digits (cc->digits cc)
-        even-digits (take-nth 2 (rest digits))
-        reverse-digits (reverse digits)
-        reverse-odds (take-nth 2 reverse-digits)
-        reverse-evens (take-nth 2 (rest reverse-digits))
-        dupped-reverse-evens (map (partial * 2) reverse-evens)
-        digit-added-dupped-reverse-evens (map sum-digits dupped-reverse-evens)]
-    (if (= 0 (mod (+ (apply + reverse-odds) (apply + digit-added-dupped-reverse-evens)) 10))
-      true
-      false)))
-
 (facts "about luhn check"
        (fact "0 passes the test"
              (valid? "0") => true)
